e2030717039e704bf29b23f2e534c2a41c15285c
Francesco Tormene <francesco.tormene@gmail.com>
2021-07-09 13:29:58 +0200

    sistemato esercizio 1

--- assets/js/esercizi-subnetting.js
+++ assets/js/esercizi-subnetting.js
@@ -6,11 +6,22 @@ soluzioni_togglers.forEach((element) => {
   element.onclick = function () {
     id = event.target.getAttribute("totoggle");
     var el = document.querySelector(id);
+    el.classList.toggle("soluzione__visibile");
     if(el.classList.contains("soluzione-semplice")){
-      el.classList.toggle("soluzione-semplice__visibile");
+      if(element.innerHTML == "Visualizza la soluzione"){
+        element.innerHTML = "Nascondi la soluzione"
+      }
+      else {
+        element.innerHTML = "Visualizza la soluzione"
+      }
     }
     if(el.classList.contains("soluzione-completa")){
-      el.classList.toggle("soluzione-completa__visibile");
+      if(element.innerHTML == "Visualizza la soluzione completa di procedimenti"){
+        element.innerHTML = "Nascondi la soluzione completa di procedimenti"
+      }
+      else {
+        element.innerHTML = "Visualizza la soluzione completa di procedimenti"
+      }
     }
     console.log("Toggled " + id);
   };
